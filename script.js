3a1e8e2d89398cfb5d034db0d3cc8931972f4900
Marcin Grabowiecki <marcin.grabowiecki@cgm.com>
2015-01-30 16:45:19 +0100

    888

--- script.js
+++ script.js
@@ -52,8 +52,14 @@ function enumerator() {
         },
         function(nr) {
             for (i = 0; i < 10; i++) {
+                var s = "" + i + "" + i + "" + i 
+                if (nr.indexOf(s) > -1) return " poczwórne " + s
+            }
+        },
+        function(nr) {
+            for (i = 0; i < 10; i++) {
                 var s = "" + i + "" + i + "" + i + "" + i
-                if (nr.indexOf(s) > -1) return " potrójne " + s
+                if (nr.substring(0, 3) > -1||nr.substring(3, 6) > -1||nr.substring(6) > -1) return " potrójne " + s
             }
         },
         function(nr) {
