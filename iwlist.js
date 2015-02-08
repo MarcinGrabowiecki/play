0082059c22b2f4e630aaa779387dd9df40a8f9f8
m <m@id867.(none)>
2015-02-08 12:56:29 +0100

    iwlist

---
+++ iwlist.js
@@ -0,0 +1,33 @@
+var expandCollapseApp = angular.module('expandCollapseApp', ['ngAnimate']);
+
+expandCollapseApp.controller('expandCollapseCtrl', function($scope, $http) {
+  $scope.active = true;
+  $scope.active1 = true;
+
+  $scope.status = "bla"
+  $scope.linie = []
+
+
+  $http.get('iwlist').
+  success(function(data, status, headers, config) {
+    $scope.status=status;
+    var l = data.split("\n")
+    var ret=[];
+    var o={};
+    for(i in l){
+      if(/Cell.[0-9]{2,}/.test(l[i])){
+        ret.push(o);
+        o={}
+      }
+      o.txt=l[i]
+      o.id=i
+      
+    }
+    $scope.linie=ret;
+    
+  }).
+  error(function(data, status, headers, config) {
+    $scope.status = status
+  });
+
+});
