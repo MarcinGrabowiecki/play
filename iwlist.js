d32b08e9167a63bc09748693fd92262f5db81ac3
m <m@id867.(none)>
2015-02-14 13:13:26 +0100

    button

--- iwlist.js
+++ iwlist.js
@@ -15,26 +15,28 @@ expandCollapseApp.controller('expandCollapseCtrl', function($scope, $http) {
     	return s.replace(/^\s+/gm,'');
     }
 
+    function lsize(s){
+    	return s.length-ltrim(s).length
+    }
 
     $scope.load = function() {
     	$scope.status = "w"
         $http.get(url).success(function(data, status, headers, config) {
             $scope.status = status;
             var r = data.split("\n").reduce(function(p,c,i){
-                console.log(ltrim(c)+" : "+c.length-ltrim(c).length)
+                console.log(c);
                 if(/Cell [0-9][0-9]/.test(c)){
-                    cell={idx:c,txt:"-"+c}
+                    var cell={idx:c,txt:"-"+c,indent:lsize(c)}
                     p.push(cell)
                 } else {
-                    cell=p.slice(-1)[0]
+                    var cell=p.slice(-1)[0]
+                    console.log(cell.indent)
+                    console.log(lsize(c))
                     //console.log(c.split(':'))
                     //console.log(cell)
                 }
-
-
                 l={idx:i}
                 l.txt=c
-                //p.push(l);
                 return p;
             },[{txt:'start'}])
             $scope.linie = r;
