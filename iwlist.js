99c1dc764df78ba386cc222b7f8b840432bfb750
m <m@id867.(none)>
2015-03-31 20:33:12 +0200

    move chart to js

--- iwlist.js
+++ iwlist.js
@@ -1,3 +1,42 @@
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+
+var ctx = document.getElementById("myChart").getContext("2d");
+// This will get the first returned node in the jQuery collection.
+var myNewChart = new Chart(ctx);
+myNewChart.Line(data);
+
+
+
+
+
+
+
+
+
 var expandCollapseApp = angular.module('expandCollapseApp', ['ngAnimate']);
 
 skany=[]
@@ -66,4 +105,4 @@ expandCollapseApp.controller('expandCollapseCtrl', function($scope, $http, $time
         });
     }
 
-});
+}); 
