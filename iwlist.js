cff8f009dc1b214062648a391e222d98ce1d3241
m <m@id867.(none)>
2015-03-31 20:24:10 +0200

    url

--- iwlist.js
+++ iwlist.js
@@ -9,8 +9,8 @@ expandCollapseApp.controller('expandCollapseCtrl', function($scope, $http, $time
     $scope.status = "bla"
     $scope.linie = []
 
-    //var url='http://mjg.dnsd.info/cgi-bin/iwlist'
-    var url='http://192.168.1.1/cgi-bin/iwlist'
+    var url='http://mjg.dnsd.info/cgi-bin/iwlist'
+    //var url='http://192.168.1.1/cgi-bin/iwlist'
 
     function ltrim(s){
     	//return x.replace(/^\s+|\s+$/gm,'');
