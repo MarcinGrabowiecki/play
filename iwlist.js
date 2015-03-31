70bcb4dcf4e20245dceba1aed1aaee89a91f2539
m <m@id867.(none)>
2015-03-31 20:45:24 +0200

    underscore

--- iwlist.js
+++ iwlist.js
@@ -25,6 +25,7 @@ var data = {
 };
 
 window.onLoad=function(){
+	console.log("bla")
 	var ctx = document.getElementById("myChart").getContext("2d");
 	// This will get the first returned node in the jQuery collection.
 	var myNewChart = new Chart(ctx);
